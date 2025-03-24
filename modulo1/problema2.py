b1aa5e1154e1c1dd0be3af9c075f4e3e999f59ea
gabriel-belo <rm551669@fiap.com.br>
2025-03-24 17:28:26 -0300

    Iniciandop o segundo problema

---
+++ modulo1/problema2.py
@@ -0,0 +1,8 @@
+#Problema- Rotação de uma lista
+import jovian
+
+#obtido pela rotação de uma lista ordenada um número desconhecido de vezes escreva uma função para
+#determinar o número mínimo de vezes que a lista ordenada original foi rotacionada para obter a
+#lista fornecida. Sua função deve ter a complexidade do pior caso de O(lg n), onde n é o
+#comprimento da lista. Você pode assumir que todos os números na lista são únicos.
+
